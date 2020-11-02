5938b73a2d97ab97797eb20da7693f1e3140a08f
Björn Daase <bjoern@daase.net>
2020-11-02 16:39:33 +0100

    Initial implementation
    Co-authored-by: Leon Matthes <leon@matthes.biz>

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+use dirs;
+use std::fs;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::path::{Path, PathBuf};
+
+pub fn compose<P: AsRef<Path>>(path: P, extension: Option<&str>) -> io::Result<File> {
+    let mut file = match fs::metadata(&path) {
+        Ok(metadata) => {
+            if metadata.len() == 0 {
+                File::create(&path)?
+            } else {
+                panic!("File already exists")
+            }
+        }
+        Err(error) => match error.kind() {
+            io::ErrorKind::NotFound => File::create(&path)?,
+            _ => panic!(
+                "Cannot read the metadata of existing file {}\nError: {}",
+                path.as_ref().display(),
+                &error
+            ),
+        },
+    };
+
+    if let Some(extension) = extension {
+        fill_file(&mut file, extension);
+    }
+    Ok(file)
+}
+
+fn fill_file(file: &mut File, extension: &str) {
+    let contents = template_contents(extension).unwrap_or_else(|| {
+        println!(
+            "No template file found for extension {}, creating an empty file",
+            &extension
+        );
+        String::default()
+    });
+
+    file.write(contents.as_bytes())
+        .expect("Unable to write file");
+}
+
+fn template_contents(extension: &str) -> Option<String> {
+    let extension_path: PathBuf = ["compose", "templates", extension].iter().collect();
+    let base_paths = [dirs::config_dir().unwrap(), dirs::data_local_dir().unwrap()];
+    let template_paths = base_paths.iter().map(|path| path.join(&extension_path));
+
+    for path in template_paths {
+        if path.exists() {
+            let contents = fs::read_to_string(&path);
+            match contents {
+                Ok(contents) => return Some(contents),
+                Err(error) => println!(
+                    "Template file {:?} could not be read!\nError: {}",
+                    &path, &error
+                ),
+            }
+        } else {
+            println!("Template file {:?} does not exist, skipping...", &path);
+        }
+    }
+    None
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+use compose;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    let filename = matches.value_of("FILENAME").unwrap();
+    compose::compose(
+        filename,
+        matches.value_of("EXTENSION").or_else(|| {
+            Path::new(filename)
+                .extension()
+                .and_then(|extension| extension.to_str())
+        }),
+    )
+    .unwrap();
 }
